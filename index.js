338731743bf28d977abf611a901eab01e6f6a6e2
Alex <muzo0@yandex.ru>
2020-09-28 01:12:30 +0300

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 let delay = (ms, value) =>
-  new Promise(resolve => {
-    setTimeout(resolve, ms, value)
-  })
+  new Promise(resolve => setTimeout(resolve, ms, value))
 
 module.exports = { delay }
